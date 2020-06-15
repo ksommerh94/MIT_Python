458c22e230a751761f33406243bc5c71fb833744
ksommerh <karensh-94@hotmail.com>
2020-06-15 16:53:21 -0500

    done!

---
+++ exercise1/problem0.py
@@ -0,0 +1,12 @@
+import numpy
+import pylab
+
+from matplotlib import pylab
+from pylab import *
+
+inputNumber1=int(input("Enter the first number: "))
+inputNumber2=int(input("Enter the second number: "))
+
+print(str(inputNumber1)+" raiser to the power of "+str(inputNumber2) +" is "+ str(pow(inputNumber1,inputNumber2)))
+
+print ("log base 2 of "+str(inputNumber1) +" is "+ str(np.log2(inputNumber1)))

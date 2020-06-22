f64dfdb640229b8d7b58d1c116d3febb07779e2d
ksommerh <karensh-94@hotmail.com>
2020-06-22 16:43:42 -0500

    First commit, excesise c

--- exercise2/ps1c.py
+++ exercise2/ps1c.py
@@ -8,5 +8,33 @@ semi_annual_raise=0.07
 r=0.04
 total_cost=1000000
 cost_down_payment=total_cost*0.25
-
 month=36
+epsilon=100
+
+annual_salary=float(input("Enter the starting salary: "))
+
+step_search=0
+
+low=0
+high=1
+guess=(low+high)/2
+
+costDownPayment=total_cost*portion_down_payment
+#monthly
+while(abs(cost_down_payment-current_savings)<epsilon):
+    current_savings=0
+    monthly_savings=(annual_salary/12)*guess
+    for i in range (36):
+        if month%6==0 and month!=0:
+            annual_salary+=annual_salary*semi_annual_raise
+            monthly_savings=(annual_salary/12)*guess
+        current_savings+=current_savings*r/12
+        current_savings+=monthly_savings
+
+
+
+
+
+
+    #print (current_savings)
+print("Number of months: " + str(month))

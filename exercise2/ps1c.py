de2a74ab54f8050ad1d44ab42f9b993bfe159b99
ksommerh <karensh-94@hotmail.com>
2020-06-22 18:03:46 -0500

    Done!

--- exercise2/ps1c.py
+++ exercise2/ps1c.py
@@ -2,16 +2,17 @@
 # You should use bisection search to help you do this efficiently. You should keep track of the number of
 # steps it takes your bisections search to finish. You should be able to reuse some of the code you wrote
 # for part B in this problem.
-import numpy
 
 semi_annual_raise=0.07
 r=0.04
 total_cost=1000000
 cost_down_payment=total_cost*0.25
-month=36
+month_initial=36
 epsilon=100
+current_savings=0
 
-annual_salary=float(input("Enter the starting salary: "))
+
+annual_salary_static=float(input("Enter the starting salary: "))
 
 step_search=0
 
@@ -19,22 +20,40 @@ low=0
 high=1
 guess=(low+high)/2
 
-costDownPayment=total_cost*portion_down_payment
-#monthly
-while(abs(cost_down_payment-current_savings)<epsilon):
-    current_savings=0
-    monthly_savings=(annual_salary/12)*guess
-    for i in range (36):
-        if month%6==0 and month!=0:
-            annual_salary+=annual_salary*semi_annual_raise
-            monthly_savings=(annual_salary/12)*guess
-        current_savings+=current_savings*r/12
-        current_savings+=monthly_savings
+
+rate_month=((r+1)**(1/12))-1
 
 
+annual_salary=annual_salary_static
+monthly_savings=(annual_salary/12)
+for month in range (month_initial):
+    if month%6==0 and month!=0:
+        annual_salary+=annual_salary*semi_annual_raise
+        monthly_savings=(annual_salary/12)*(1+rate_month)
+    current_savings+=current_savings*rate_month
+    current_savings+=monthly_savings
+if current_savings<cost_down_payment:
+    print("It is not possible to pay the down payment in three years.")
+else:
+    while(abs(cost_down_payment-current_savings)>epsilon):
+        annual_salary=annual_salary_static
+        current_savings=0
+        guess=(low+high)/2
+        monthly_savings=(annual_salary/12)*guess
 
+        for month in range (month_initial):
+            if month%6==0 and month!=0:
+                annual_salary+=annual_salary*semi_annual_raise
+                #monthly_savings=(annual_salary/12)*(1+rate_month)
+                monthly_savings=(annual_salary/12)*guess
+            current_savings+=current_savings*rate_month
+            current_savings+=monthly_savings
 
+        if cost_down_payment<current_savings:
+            high=guess
+        else:
+            low=guess
+        step_search+=1
 
-
-    #print (current_savings)
-print("Number of months: " + str(month))
+    print("Best savings rate: " + str(round(guess, 4)))
+    print("Steps in bisection search: " + str(step_search))
